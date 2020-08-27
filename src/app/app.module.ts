4ead1623b5aa5dca7e6466030743f0c1c475715d
Rishi Dubey <rishidubey745@gmail.com>
2020-08-27 21:51:27 +0530

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { EventsComponent } from './components/events/events.component';
+import { WorkshopsComponent } from './components/workshops/workshops.component';
+import { SpeakersComponent } from './components/speakers/speakers.component';
+import { HighlightsComponent } from './components/highlights/highlights.component';
+import { ConnexionsComponent } from './components/connexions/connexions.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    AboutUsComponent,
+    EventsComponent,
+    WorkshopsComponent,
+    SpeakersComponent,
+    HighlightsComponent,
+    ConnexionsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
